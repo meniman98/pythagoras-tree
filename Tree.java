01edc8352f1f8456db349b418b7c6cf165d85402
Huncho Jack <meniman98@gmail.com>
2022-12-16 21:03:40 +0100

    succeeded

--- Tree.java
+++ Tree.java
@@ -7,26 +7,26 @@ import java.awt.geom.Path2D;
 public class Tree extends JComponent {
 
     @Override
-    protected void paintComponent(Graphics g) {
-        drawTree((Graphics2D) g, 300, 450, 400, 450, 0);
+    protected void paintComponent(Graphics graphics) {
+        drawPythagorasTree((Graphics2D) graphics, 300, 450, 400, 450, 0);
     }
 
-    private void drawTree(Graphics2D g, float x1, float y1, float x2, float y2,
+    void drawPythagorasTree(Graphics2D graphics, float x1, float y1, float x2, float y2,
                           int depth) {
 
-//        If
+//        If depth is too low, stop the recursion
         if (depth == 10)
             return;
 
-        float dx = x2 - x1;
-        float dy = y1 - y2;
+        float differenceInX = x2 - x1;
+        float differenceInY = y1 - y2;
 
-        float x3 = x2 - dy;
-        float y3 = y2 - dx;
-        float x4 = x1 - dy;
-        float y4 = y1 - dx;
-        float x5 = x4 + 0.5F * (dx - dy);
-        float y5 = y4 - 0.5F * (dx + dy);
+        float x3 = x2 - differenceInY;
+        float y3 = y2 - differenceInX;
+        float x4 = x1 - differenceInY;
+        float y4 = y1 - differenceInX;
+        float x5 = x4 + 0.5F * (differenceInX - differenceInY);
+        float y5 = y4 - 0.5F * (differenceInX + differenceInY);
 
         Path2D square = new Path2D.Float();
         square.moveTo(x1, y1);
@@ -36,8 +36,8 @@ public class Tree extends JComponent {
         square.closePath();
 
 
-        g.fill(square);
-        g.draw(square);
+        graphics.fill(square);
+        graphics.draw(square);
 
         Path2D triangle = new Path2D.Float();
         triangle.moveTo(x3, y3);
@@ -46,10 +46,10 @@ public class Tree extends JComponent {
         triangle.closePath();
 
 
-        g.fill(triangle);
-        g.draw(triangle);
+        graphics.fill(triangle);
+        graphics.draw(triangle);
 
-        drawTree(g, x4, y4, x5, y5, depth + 1);
-        drawTree(g, x5, y5, x3, y3, depth + 1);
+        drawPythagorasTree(graphics, x4, y4, x5, y5, depth + 1);
+        drawPythagorasTree(graphics, x5, y5, x3, y3, depth + 1);
     }
 }
